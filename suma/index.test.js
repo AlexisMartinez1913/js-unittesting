ed63df9b260119ba43738612654f8b4a07c6cca1
Alexis Martinez <jhonyalexis100@gmail.com>
2024-02-23 16:10:03 -0500

    learning test with js

--- suma/index.test.js
+++ suma/index.test.js
@@ -1,4 +1,6 @@
+const { default: expect } = require("expect");
 const sum = require("./index.js");
+const { multiply } = require("../functions.js");
 
 /*test("Add numbers", function() {
   //ARRANGE
@@ -16,11 +18,11 @@ const sum = require("./index.js");
 describe('Sum Function test suite', () => {
   it('when I sent 2 numbers returned the sum of them', () => {
 
-    //ARRANGE
+    //ARRANGE -> Inicializa los objetos y establece los valores q vamos a usar en el test
     const a = 1;
     const b = 2;
 
-    //ACT
+    //ACT -> Realiza la llamada al metodo a probar con los parametross
     const result = sum(a, b);
 
     //ARRANGE
@@ -51,5 +53,27 @@ describe('Sum Function test suite', () => {
 
     //ARRANGE
     expect(result).toBe('null1');
+  })
+  it('should be return multiple when i sent two number', () => {
+    //ARRANGE -> Inicializa los objetos y establece los valores q vamos a usar en el test
+    const number1 = 4
+    const number2 = 3
+
+    //ACT -> Realiza la llamada al metodo a probar con los parametross
+    const result = multiply(number1, number2)
+
+    //ASSERT -> Comprueba q el metodo de pruebas se comporta como se espera
+    expect(result).toBe(12)
+  })
+  it('Should be return sum when i sent two numbers', () => {
+    //arrange
+    const num1 = 6
+    const num2 = 1
+
+    //ACT
+    const total = sum(num1, num2)
+
+    //ASSERT
+    expect(total).toBe(7)
   })
 })
